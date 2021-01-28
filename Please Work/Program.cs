834f41d303d0dd80975a439cca0ce094931809e2
Роберт Мартиросян <flyberg.martirosyan@gmail.com>
2021-01-29 00:09:04 +0300

    Добавьте файлы проекта.

---
+++ Please Work/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Please_Work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
